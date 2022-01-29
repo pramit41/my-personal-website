9128798882aff554bfb2650cb6065127a98fcb1b
Pramit Thapa <69735326+pramit41@users.noreply.github.com>
2022-01-29 17:30:37 -0500

    first commit

---
+++ src/componenets/Nav.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./nav.css"
+
+export default function Nav(){
+    return(
+        <nav className='nav'>
+          <h1>Hi! I am Pramit Thapa</h1>
+          <ul className='nav-items'>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Resume</li>
+          </ul>
+        </nav>
+    )
+}
